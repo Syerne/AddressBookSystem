683f61119fbfdf6c3784f803e5ef4bfd0f406906
Syerne <shubhamyerne1994@gmail.com>
2022-06-29 18:40:11 +0530

    Welcome to the Employee Address Book System

---
+++ EmpAddressBook.java
@@ -0,0 +1,10 @@
+package com.blz.profile;
+
+public class EmpAddressBook {
+    public static void main(String[] args) {
+        displayMessage();
+    }
+    public static void displayMessage(){
+        System.out.println("Welcome to Bridge Employee Address Book");
+    }
+}
