100e237139b2433ddd0a202eac7755dd38c8b678
Syerne <shubhamyerne1994@gmail.com>
2022-06-30 11:25:28 +0530

    Added : Create Contacts

--- EmpAddressBook.java
+++ EmpAddressBook.java
@@ -1,8 +1,12 @@
 package com.blz.profile;
 
+import com.blz.profile.dto.Contacts;
+
 public class EmpAddressBook {
     public static void main(String[] args) {
         displayMessage();
+        Contacts contacts = new Contacts();
+        System.out.println(contacts.toString());
     }
     public static void displayMessage(){
         System.out.println("Welcome to Bridge Employee Address Book");
